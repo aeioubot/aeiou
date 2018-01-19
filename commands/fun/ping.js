de001b067d07afb711f5ec54db7d2428a0787a97
송서연 <southkorea@protonmail.com>
2018-01-20 08:28:49 +0900

    Nothing to be seen here.

--- commands/fun/ping.js
+++ commands/fun/ping.js
@@ -4,7 +4,7 @@ module.exports = class PingCommand extends commando.Command {
 	constructor(client) {
 		super(client, {
 			name: 'ping',
-			group: 'util',
+			group: 'fun',
 			memberName: 'ping',
 			description: 'Checks the bot\'s ping to the Discord server.',
 			throttling: {
@@ -17,7 +17,7 @@ module.exports = class PingCommand extends commando.Command {
 	async run(msg) {
 		try {
 			const placeholder = await msg.say("Pinging...");
-			return placeholder.edit(`Pong! Yui's ping is \`${placeholder.createdTimestamp - msg.createdTimestamp}ms\`. ${this.client.ping ? `The websocket ping is \`${Math.round(this.client.ping)}ms.\`` : ''}`);
+			return placeholder.edit(`Pong! Aeiou's ping is \`${placeholder.createdTimestamp - msg.createdTimestamp}ms\`. ${this.client.ping ? `The websocket ping is \`${Math.round(this.client.ping)}ms.\`` : ''}`);
 		} catch(ex) {
 			//
 		}

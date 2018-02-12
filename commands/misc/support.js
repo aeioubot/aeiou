b211dbc3ef2fb67f5030e1b5faca6d11da16bc96
Kuwar Nagpal <kuwar.nagpal@gmail.com>
2018-02-11 21:32:04 -0500

    Added a support command to DM an invite, a say command, and a command to change nickname on this guild.

---
+++ commands/misc/support.js
@@ -0,0 +1,24 @@
+const {Command} = require('discord.js-commando');
+
+module.exports = class ReplyCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'support',
+			group: 'misc',
+			memberName: 'support',
+			description: 'DMs you a link to the support server.',
+			details: 'DMs you a link to the support server.',
+		});
+	}
+
+	async run(msg) {
+		const test = await msg.say("I'm DMing an invite to you!");
+		return msg.author.send("Here's an invite to the support server: \n https://discord.gg/JB8xdT5")
+			.then(() => {
+				setTimeout(() => test.delete(), 1000);
+			})
+			.catch(() => {
+				test.edit("I wasn't able to DM you...");
+			});
+	}
+};

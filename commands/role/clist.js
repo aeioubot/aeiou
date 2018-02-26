13fa199cfefa6505e9a61c9c682f3972e76591ac
Kuwar Nagpal <kuwar.nagpal@gmail.com>
2018-02-26 18:04:08 -0500

    Fixed Dlist for absent members, fixed restartall command.

--- commands/role/clist.js
+++ commands/role/clist.js
@@ -19,9 +19,7 @@ module.exports = class CListCommand extends Command {
 		const donors = await donorDB.getDonors(msg);
 		if (donors.length < 1) return msg.say('This server has no donor colors.');
 		let message = 'Here are the current custom role owners, and the colors they are assigned to:\n```';
-		donors.forEach((donor) => {
-			message += `${msg.guild.members.find('id', donor.id).user.username.padEnd(35)}${msg.guild.roles.find('id', donor.role).name}\n`;
-		});
+		donors.forEach((donor) => message += `${msg.guild.members.find('id', donor.id) ? msg.guild.members.find('id', donor.id).user.username.padEnd(35) : `ID: ${donor.id}`.padEnd(35)}${msg.guild.roles.find('id', donor.role).name}\n`);
 		message += '```';
 		msg.say(message);
 	}
--- managerCommands/restart.js
+++ managerCommands/restart.js
@@ -1,14 +1,13 @@
 const child = require('child_process');
 
 module.exports = async (manager) => {
-	manager.respawn = false;
-	console.log('Pulling...');
-	child.execSync('git pull');
-	console.log('Pulling complete! Installing...');
-	child.execSync('npm install --production --silent');
-	console.log('Install complete! Killing children.');
-	manager.broadcastEval('process.exit(0)').then(() => {
-		console.log('Children dead, killing self.');
-		process.exit(0);
+	require('../utils/models/creact.js').buildReactCache().then(() => {
+		console.log('[Manager] Reactions cache built!');
+		console.log('[Manager] Pulling...');
+		child.execSync('git pull');
+		console.log('[Manager] Pulling complete! Installing...');
+		child.execSync('npm install --production --silent');
+		console.log('[Manager] Install complete! Killing children.');
+		manager.broadcastEval('process.exit(0)');
 	});
 };

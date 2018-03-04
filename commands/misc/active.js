bee3b5d7624b29e00e187fb7e229b15ec3e2f7d9
Kuwar Nagpal <kuwar.nagpal@gmail.com>
2018-03-04 13:47:26 -0500

    Added active command

---
+++ commands/misc/active.js
@@ -0,0 +1,35 @@
+const {Command} = require('discord.js-commando');
+
+module.exports = class ReplyCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'active',
+			group: 'misc',
+			memberName: 'active',
+			description: 'Shows the members who have been active in the past 5 minutes.',
+			details: 'Shows the members who have been active in the past 5 minutes.',
+			examples: ['active', 'active 10'],
+			format: '[minutes]',
+			guildOnly: true,
+			args: [
+				{
+					key: 'minutes',
+					prompt: 'kerchoo pop skibi di pap pap',
+					type: 'integer',
+					default: '5',
+				},
+			],
+		});
+	}
+
+	async run(msg, { minutes }) {
+		const sayArray = [`Members that have been active in the past **${minutes}** minutes:\`\`\``];
+		const checkMS = minutes * 60000;
+		const now = Date.now();
+		msg.guild.members.forEach((m) => {
+			if (m.lastMessage && !m.user.bot && now - m.lastMessage.createdTimestamp <= checkMS) sayArray.push(m.user.tag);
+		});
+		sayArray.push('```');
+		sayArray.length === 3 ? msg.say(`Members that have been active in the past **${minutes}** minutes:\n\`\`\`Just you!\`\`\``) : msg.say(sayArray.join('\n'));
+	}
+};

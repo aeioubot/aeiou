a862a005b0e4c05f028f1e9b13262eace9c65909
Kuwar Nagpal <kuwar.nagpal@gmail.com>
2018-03-04 11:52:22 -0500

    Plant status meme for Aeiou

--- commands/plant/status.js
+++ commands/plant/status.js
@@ -90,6 +90,20 @@ module.exports = class ReplyCommand extends Command {
 	}
 
 	async run(msg, {notMe}) {
+		if (notMe.id == '309024868530257920') {
+			let embed = {
+				title: 'W̷̝͍̜̩̲̣͓̝͟h̵̢͉̘͍͟a͏̬̯̯̳t̟̭̝̙̀ ̸͔̙̯͎͍̹̗g̸͏̼̯̩̭̠̟͚r̵̲̟̕͘e̷̙̼͓͚̬e̛̦̖̤̺͚͖͓̣͠n̶͔̤̭̕e͍̼͇̰̖̫̤r̹̩̲̮̙̩̰͠ỳ͇̺̘͘?̯̟̦̮̼͔̟͡',
+				description: 'AEIOU NEEDS NO PLANT, FOR AEIOU IS **ALL** THAT IS PLANT.',
+				image: {
+					url: 'https://i.ytimg.com/vi/hSIzkVdlNBQ/maxresdefault.jpg',
+				},
+				author: {
+					name: 'GOD EMPEROR AEIOU',
+					icon_url: notMe.user.displayAvatarURL,
+				},
+			};
+			msg.say('You fool.', {embed});
+		}
 		const plantData = await plants.getPlant(notMe ? notMe.id : msg).then((plant) => plant.getPlantData());
 		if (!plantData.activeSeed) return msg.say(`${notMe ? `${notMe.displayName} doesnt` : 'You don\'t'} have a seed planted currently.`);
 

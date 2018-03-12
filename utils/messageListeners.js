83c4f87ef23408e20e9da7d5e6443f94a8ab191d
Niek <enteryournamehere@users.noreply.github.com>
2018-03-12 18:07:05 +0100

    markdown copy thing

--- utils/messageListeners.js
+++ utils/messageListeners.js
@@ -12,76 +12,62 @@ function upify(msgContent) {
 	return msgContent.join('');
 }
 
-function determineMode(msgContent) {
-	if (msgContent.slice(0, 3) == '***' && msgContent.slice(-3) == '***' && msgContent === msgContent.toUpperCase()) return 'boldItalicsUppercase';
-	if (msgContent.slice(0, 3) == '***' && msgContent.slice(-3) == '***') return 'boldItalics';
-	if (msgContent.slice(0, 2) == '**' && msgContent.slice(-2) == '**') return 'bold';
-	if ((msgContent.slice(0, 1) == '*' && msgContent.slice(-1) == '*') || (msgContent.slice(0, 1) == '_' && msgContent.slice(-1) == '_')) return 'italics';
-	if (msgContent === msgContent.toUpperCase()) return 'uppercase';
-	return 'none';
-}
-translations = {
-	'none': 0,
-	'uppercase': 0,
-	'italics': 1,
-	'bold': 2,
-	'boldItalics': 3,
-	'boldItalicsUppercase': 3,
-};
-
 module.exports = {
 	creact: async (msg) => {
 		if (msg.author.bot || msg.channel.type != 'text' || msg.client.provider.get(msg.guild, 'ignoredChannels', []).includes(msg.channel.id)) return;
 		const reactionObjects = reactDB.allGuildReactions[msg.guild.id] || [];
-		let mode = determineMode(msg.content);
-		msg.content = msg.content.substring(translations[mode], msg.content.length - translations[mode]);
+		let i = 0;
+		let markdownStart = '';
+		while (msg.content[i] == msg.content[msg.content.length - i - 1] && '*_~'.indexOf(msg.content[i]) > -1) {
+			markdownStart += msg.content[i];
+			i++;
+		}
+		console.log([i, msg.content, msg.content.length - i]);
+		let triggerContent = msg.content.substr(i, msg.content.length - (2 * i));
+		console.log(triggerContent);
 		let toSay = reactionObjects.find((reactObject) => {
-			if (msg.content.toLowerCase() === reactObject.trigger) return reactObject;
+			if (triggerContent.toLowerCase() === reactObject.trigger) return reactObject;
 		});
 		if (toSay) {
-			if (mode === 'boldItalicsUppercase') return msg.channel.send(`***${upify(toSay.content)}***`).catch(() => {});
-			if (mode === 'uppercase')            return msg.channel.send(upify(toSay.content)).catch(() => {});
-			if (mode === 'boldItalics')          return msg.channel.send(`***${toSay.content}***`).catch(() => {});
-			if (mode === 'bold')                 return msg.channel.send(`**${toSay.content}**`).catch(() => {});
-			if (mode === 'italics')              return msg.channel.send(`_${toSay.content}_`).catch(() => {});
-			return msg.channel.send(toSay.content).catch(() => {});
+			let reactContent = (triggerContent === upify(triggerContent)) ? toSay.content.toUpperCase() : toSay.content;
+			return msg.channel.send(markdownStart + reactContent + markdownStart.split('').reverse().join(''));
 		}
 	},
 	plantSeed: async (msg) => {
 		if (Math.floor(Math.random() * 600) !== 0 || msg.client.provider.get(msg.guild.id, 'noSeedChannels', []).includes(msg.channel.id)) return;
 		msg.react('🌰')
-		.then(() => {
-			setTimeout(async () => {
-				const earners = Array.from(msg.reactions.get('🌰').users.keys());
-				const errorTest = await msg.react('⏰').catch(() => 'error');
-				if (errorTest == 'error') return;
-				earners.splice(earners.indexOf(msg.client.user.id), 1);
-				earners.forEach((userID) => {
-					plants.getPlant(userID).then((plantClass) => {
-						const color = Math.floor(Math.random() * 3);
-						plantClass.addToSeeds({
-							name: `Seed dropped by ${msg.member.displayName}`,
-							growthRate: Math.floor(Math.random() * 10) + 11,
-							leafiness: Math.floor(Math.random() * 15) + 16,
-							waterAffinity: 5,
-							red: color === 0 ? 255 : 0,
-							green: color === 1 ? 255 : 0,
-							blue: color === 2 ? 255 : 0,
+			.then(() => {
+				setTimeout(async () => {
+					const earners = Array.from(msg.reactions.get('🌰').users.keys());
+					const errorTest = await msg.react('⏰').catch(() => 'error');
+					if (errorTest == 'error') return;
+					earners.splice(earners.indexOf(msg.client.user.id), 1);
+					earners.forEach((userID) => {
+						plants.getPlant(userID).then((plantClass) => {
+							const color = Math.floor(Math.random() * 3);
+							plantClass.addToSeeds({
+								name: `Seed dropped by ${msg.member.displayName}`,
+								growthRate: Math.floor(Math.random() * 10) + 11,
+								leafiness: Math.floor(Math.random() * 15) + 16,
+								waterAffinity: 5,
+								red: color === 0 ? 255 : 0,
+								green: color === 1 ? 255 : 0,
+								blue: color === 2 ? 255 : 0,
+							});
+							plants.storePlant(plantClass);
 						});
-						plants.storePlant(plantClass);
 					});
-				});
-			}, 10000);
-		})
-		.catch((e) => {
-			if (e.message == 'Reaction blocked') return;
-			let m = msg.client.provider.get(msg.guild.id, 'noSeedChannels', []);
-			m.push(msg.channel.id);
-			msg.client.provider.set(
-				msg.guild.id,
-				'noSeedChannels',
-				m
-			);
-		});
+				}, 10000);
+			})
+			.catch((e) => {
+				if (e.message == 'Reaction blocked') return;
+				let m = msg.client.provider.get(msg.guild.id, 'noSeedChannels', []);
+				m.push(msg.channel.id);
+				msg.client.provider.set(
+					msg.guild.id,
+					'noSeedChannels',
+					m
+				);
+			});
 	},
 };

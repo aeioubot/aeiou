8648ea14791187b6e35b2804f3832a672cf6a8dc
Niek <enteryournamehere@users.noreply.github.com>
2018-03-12 18:49:31 +0100

    My previous commit (83c4f87ef23408e20e9da7d5e6443f94a8ab191d) and this commit combined are meant to improve the system which is used to copy the markdown a user uses around a custom reaction trigger. This means that if there is a custom reaction with the trigger "trigger" and response "response", and a user types **trigger**, **reponse** is said, and __**TRIGGER**__ would return __**RESPONSE**__, et cetera. The specific method used to achieve this result is further explained in the comments which you can find in the code in util/messageListeners.js.

--- utils/messageListeners.js
+++ utils/messageListeners.js
@@ -16,20 +16,24 @@ module.exports = {
 	creact: async (msg) => {
 		if (msg.author.bot || msg.channel.type != 'text' || msg.client.provider.get(msg.guild, 'ignoredChannels', []).includes(msg.channel.id)) return;
 		const reactionObjects = reactDB.allGuildReactions[msg.guild.id] || [];
-		let i = 0;
+		// Define the variable used to keep track of the markdown the user uses at the start and end of their message.
 		let markdownStart = '';
-		while (msg.content[i] == msg.content[msg.content.length - i - 1] && '*_~'.indexOf(msg.content[i]) > -1) {
+		// Run a loop to add the user's markdown (which can contain *, _ and ~ characters) to the markdownStart variable.
+		// This will keep running until the markdown at the start of the message is different from the markdown at the end of the message reversed.
+		for (let i = 0; msg.content[i] == msg.content[msg.content.length - i - 1] && '_*~'.indexOf(msg.content[i]) > -1; i++) {
+			// Add the markdown character to the markdownStart variable.
 			markdownStart += msg.content[i];
-			i++;
 		}
-		console.log([i, msg.content, msg.content.length - i]);
+		// This variable holds the actual custom reaction trigger, without the markdown the user uses.
 		let triggerContent = msg.content.substr(i, msg.content.length - (2 * i));
-		console.log(triggerContent);
+		// Find the response for the trigger.
 		let toSay = reactionObjects.find((reactObject) => {
 			if (triggerContent.toLowerCase() === reactObject.trigger) return reactObject;
 		});
 		if (toSay) {
+			// If the triggerContent is the same as the triggerContent to upper case, send the response in uppercase too.
 			let reactContent = (triggerContent === upify(triggerContent)) ? toSay.content.toUpperCase() : toSay.content;
+			// Send the custom reaction with the user's markdown around it.
 			return msg.channel.send(markdownStart + reactContent + markdownStart.split('').reverse().join(''));
 		}
 	},

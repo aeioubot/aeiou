58a004b8cec479484b6131fb2ff8c74db812ea29
dmbutalsodm <Kuwar.nagpal@gmail.com>
2018-03-14 12:37:37 -0400

    Water tray renamed to drainage pot

--- utils/classes/plantItems/DrainagePot.js
+++ utils/classes/plantItems/DrainagePot.js
@@ -1,11 +1,11 @@
 module.exports = class WaterTray {
 	constructor() {
-		this.name = 'Water tray';
+		this.name = 'Drainage pot';
 		this.cost = 75;
 		this.description = 'Prevents your plant from being overwatered. Has a chance to break on use.';
 	}
 
 	use(plant, msg) {
-		return msg.say('The water tray is automatically applied. It wont last forever, so check back often.');
+		return msg.say('The drainage pot is automatically applied. It wont last forever, so check back often.');
 	}
 };

4c6276ce799d2dd70cb53f162a303f6aadca1eb6
dmbutalsodm <Kuwar.nagpal@gmail.com>
2018-03-28 07:54:43 -0400

    Switched back APIs to test API ban

--- commands/search/image.js
+++ commands/search/image.js
@@ -12,7 +12,8 @@ module.exports = class YoutubeCommand extends Command {
 			aliases: ['i', 'img'],
 			examples: ['image kodak black', 'image weeb stuff'],
 			format: '[query]',
-			guildOnly: false,
+			guildOnly: true,
+			throttling: {usages: 1, duration: 30},
 			args: [
 				{
 					key: 'query',
@@ -31,25 +32,25 @@ module.exports = class YoutubeCommand extends Command {
 					title: `Image result for "${query}"`,
 					color: 0x4885ED,
 					image: {
-						url: this.data.splice(0, 1)[0].url,
+						url: this.data.result.items.splice(0, 1)[0].media,
 					},
 				}});
 			} catch (e) {
 				return msg.say('There are no more results for this search.').catch(() => {});
 			}
-			msg.member.currentSearch = msg.channel.createMessageCollector((m) => m.author.id == msg.author.id && m.channel.id == msg.channel.id && m.content.toLowerCase() == 'next', {time: 30000, maxMatches: 1});
+			msg.member.currentSearch = msg.channel.createMessageCollector((m) => m.author.id == msg.author.id && m.channel.id == msg.channel.id && m.content.toLowerCase() == 'next', {time: 120000, maxMatches: 1});
 			msg.member.currentSearch.on('collect', () => sayResult());
 			return;
 		};
 
 		request({
-			uri: `http://api.ababeen.com/api/images.php?count=10&q=${query}`,
+			uri: `https://api.qwant.com/api/search/images?count=10&offset=1&q=${query}`,
 			json: true,
 			headers: {
-				'User-Agent': 'Aeiou Bot',
+				'User-Agent': `${new Date().getTime()}`,
 			},
 		}).then((d) => {
-			this.data = d;
+			this.data = d.data;
 			sayResult();
 		}).catch((e) => {
 			console.log(e);

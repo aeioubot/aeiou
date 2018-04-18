808760e5a009f9c5234e22b912b5b8cbcae2cb43
dmbutalsodm <Kuwar.nagpal@gmail.com>
2018-04-18 13:22:22 -0400

    Made ping more fun

--- commands/fun/ping.js
+++ commands/fun/ping.js
@@ -1,4 +1,37 @@
 const commando = require('discord.js-commando');
+const responses = [
+	'hi',
+	'Hello!!',
+	'Henlo!',
+	'Hi there.',
+	'aaaaaa',
+	'I\'m listening.',
+	'Yes?',
+	'Hi, how are you?',
+	'Hey there.',
+	'yes hi its me',
+	'yes hello this is aeiou',
+	'Hi, I\'m Aeiou.',
+	'Greetings!!',
+	'I am here!',
+	'Ready to serve!',
+	'Pong!',
+	'its a school night',
+	'ATTENTION REQUESTED?',
+	'Yes, I am here.',
+	'Yes, that\'s me!',
+	'What\'s good?',
+	'Yeah?',
+	'I read you.',
+	'I hear you.',
+	'Watching!',
+	'You got me.',
+	'I\'m not sleeping!!!',
+	'\\*krrt* Aeiou to Earth, are you reading?',
+	'You called?',
+	'I\'m alive!',
+	'Hearing you, loud and clear!',
+];
 
 module.exports = class PingCommand extends commando.Command {
 	constructor(client) {
@@ -7,19 +40,25 @@ module.exports = class PingCommand extends commando.Command {
 			group: 'fun',
 			memberName: 'ping',
 			description: 'Checks the bot\'s ping to the Discord server.',
+			aliases: ['hey', 'hi', 'yo', 'hello', 'pong', 'pyong'],
 			throttling: {
 				usages: 5,
 				duration: 10,
 			},
+			args: [
+				{
+					key: 'detailed',
+					prompt: 'hello how are you',
+					type: 'string',
+					default: '',
+				},
+			],
 		});
 	}
 
-	async run(msg) {
-		try {
-			const placeholder = await msg.say('Pinging...');
-			return placeholder.edit(`Pong! Aeiou's ping is \`${placeholder.createdTimestamp - msg.createdTimestamp}ms\`. ${this.client.ping ? `The websocket ping is \`${Math.round(this.client.ping)}ms.\`` : ''}`);
-		} catch (ex) {
-			//
-		}
+	async run(msg, {detailed}) {
+		if (!detailed) return msg.say(responses[Math.floor(responses.length * Math.random())]);
+		const placeholder = await msg.say('Pinging...');
+		return placeholder.edit(`Pong! Aeiou's ping is \`${placeholder.createdTimestamp - msg.createdTimestamp}ms\`. ${this.client.ping ? `The websocket ping is \`${Math.round(this.client.ping)}ms.\`` : ''}`);
 	}
 };

e61a461687a9c7b87ddf31e0a5549258d630c9a1
Niek <enteryournamehere@users.noreply.github.com>
2018-04-28 23:08:06 +0200

    critical bug in restart.js 🌰⏰

--- commands/owner/restart.js
+++ commands/owner/restart.js
@@ -28,19 +28,20 @@ module.exports = class RestartCommand extends Command {
 
 	async run(msg, {all}) {
 		if (all == 'all') {
-			await msg.react('⌛');
+			await msg.react('🌰');
 			console.log(`[Shard ${this.client.shard.id}] Pulling...`);
 			child.execSync('git pull');
 			console.log(`[Shard ${this.client.shard.id}] Pulling complete! Installing...`);
 			child.execSync('npm install --production --silent');
 			console.log(`[Shard ${this.client.shard.id}] Install complete! Killing my friends.`);
-			return msg.react('✅').then(() => process.send(new GatewayCommand(
+			return msg.react('⏰').then(() => process.send(new GatewayCommand(
 				this.client.shard.count,
 				this.client.shard.id,
 				'restart',
 			)));
 		}
+		msg.react('🌰');
 		console.log(`[Shard ${msg.client.shard.id}] restarting...`);
-		return msg.react('✅').then(() => process.exit(0));
+		return msg.react('⏰').then(() => process.exit(0));
 	}
 };

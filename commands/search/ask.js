2805b1241398c7ea90c50bb39dce99c17af48bed
dmbutalsodm <Kuwar.nagpal@gmail.com>
2018-04-28 15:50:22 -0400

    Ask command caches 100 questions

--- commands/search/ask.js
+++ commands/search/ask.js
@@ -14,13 +14,42 @@ module.exports = class AskCommand extends Command {
 	}
 
 	async run(msg, args) {
-		return request('http://www.reddit.com/r/askReddit/hot/.json?count=50', {json: true}).then((questions) => {
-			msg.channel.startTyping();
-			return msg.say(
-				questions.data.children.filter((e) => !e.data.stickied)[Math.floor(Math.random() * questions.data.children.length)].data.title
-					.replace(/reddit/gi, 'Discord')
-					.replace(/\[serious\]/gi, '')
-			).then(msg.channel.stopTyping());
-		}).catch(() => msg.say('Something went wrong, try again later.')).catch(() => {});
+		if (!this.questions || !this.questions.length) {
+			setTimeout(() => this.questions = [], 21600000); // Delete old questions after 6 hours
+			return request('http://www.reddit.com/r/askReddit/hot/.json?count=25', {json: true}).then((questions) => { // Cache 100 questions;
+				this.questions = questions.data.children.filter((e) => !e.data.stickied && !e.data.over_18);
+				return questions.data.before;
+			}).then(async t => {
+				await msg.say(
+					this.questions.splice(Math.floor(Math.random() * this.questions.length), 1)[0].data.title
+						.replace(/redditors/gi, 'Discord users')
+						.replace(/subreddit/gi, 'server')
+						.replace(/reddit/gi, 'Discord')
+						.replace(/\[serious\]/gi, '')
+				);
+				return t;
+			}).then((t) => {
+				return request(`http://www.reddit.com/r/askReddit/hot/.json?count=25&after=${t}`, {json: true}).then((questions) => {
+					this.questions = this.questions.concat(questions.data.children.filter((e) => !e.data.stickied && !e.data.over_18));
+					return questions.data.before;
+				});
+			}).then((t) => {
+				return request(`http://www.reddit.com/r/askReddit/hot/.json?count=25&after=${t}`, {json: true}).then((questions) => {
+					this.questions = this.questions.concat(questions.data.children.filter((e) => !e.data.stickied && !e.data.over_18));
+					return questions.data.before;
+				});
+			}).then((t) => {
+				return request(`http://www.reddit.com/r/askReddit/hot/.json?count=25&after=${t}`, {json: true}).then((questions) => {
+					this.questions = this.questions.concat(questions.data.children.filter((e) => !e.data.stickied && !e.data.over_18));
+					return;
+				});
+			});
+		}
+		return msg.say(
+			this.questions.splice(Math.floor(Math.random() * this.questions.length), 1)[0].data.title
+				.replace(/subreddit/gi, 'server')
+				.replace(/reddit/gi, 'Discord')
+				.replace(/\[serious\]/gi, '')
+		);
 	}
 };

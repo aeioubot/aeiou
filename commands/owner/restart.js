0508e2918a05ac84703c6c556563dfb3ac741765
dmbutalsodm <Kuwar.nagpal@gmail.com>
2018-04-28 12:49:37 -0400

    Changed restart logic

--- commands/owner/restart.js
+++ commands/owner/restart.js
@@ -1,5 +1,6 @@
 const {Command} = require('discord.js-commando');
 const GatewayCommand = require('../../utils/classes/GatewayCommand.js');
+const child = require('child_process');
 
 module.exports = class RestartCommand extends Command {
 	constructor(client) {
@@ -27,13 +28,19 @@ module.exports = class RestartCommand extends Command {
 
 	async run(msg, {all}) {
 		if (all == 'all') {
+			await msg.react('⌛');
+			console.log(`[Shard ${this.client.shard.id}] Pulling...`);
+			child.execSync('git pull');
+			console.log(`[Shard ${this.client.shard.id}] Pulling complete! Installing...`);
+			child.execSync('npm install --production --silent');
+			console.log(`[Shard ${this.client.shard.id}] Install complete! Killing my friends.`);
 			return msg.react('✅').then(() => process.send(new GatewayCommand(
 				this.client.shard.count,
 				this.client.shard.id,
 				'restart',
 			)));
 		}
-		console.log(`Shard ${msg.client.shard.id} restarting...`);
+		console.log(`[Shard ${msg.client.shard.id}] restarting...`);
 		return msg.react('✅').then(() => process.exit(0));
 	}
 };

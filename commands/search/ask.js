510f37e645cc435bb4d7acb9af9931c9c4578855
dmbutalsodm <Kuwar.nagpal@gmail.com>
2018-05-02 13:02:12 -0400

    Fixed ask command

--- commands/search/ask.js
+++ commands/search/ask.js
@@ -16,37 +16,24 @@ module.exports = class AskCommand extends Command {
 	async run(msg, args) {
 		if (!this.questions || !this.questions.length) {
 			setTimeout(() => this.questions = [], 21600000); // Delete old questions after 6 hours
-			return request('http://www.reddit.com/r/askReddit/hot/.json?count=25', {json: true}).then((questions) => { // Cache 100 questions;
-				this.questions = questions.data.children.filter((e) => !e.data.stickied && !e.data.over_18);
+			return request('http://www.reddit.com/r/askReddit/hot/.json?limit=100', {json: true}).then((questions) => { // Cache 100 questions;
+				this.questions = questions.data.children.filter((e) => !e.data.stickied && !e.data.over_18).map(e => e.data.title);
 				return questions.data.before;
 			}).then(async t => {
 				await msg.say(
-					this.questions.splice(Math.floor(Math.random() * this.questions.length), 1)[0].data.title
+					this.questions.splice(Math.floor(Math.random() * this.questions.length), 1)[0]
 						.replace(/redditors/gi, 'Discord users')
 						.replace(/subreddit/gi, 'server')
 						.replace(/reddit/gi, 'Discord')
 						.replace(/\[serious\]/gi, '')
 				);
 				return t;
-			}).then((t) => {
-				return request(`http://www.reddit.com/r/askReddit/hot/.json?count=25&after=${t}`, {json: true}).then((questions) => {
-					this.questions = this.questions.concat(questions.data.children.filter((e) => !e.data.stickied && !e.data.over_18));
-					return questions.data.before;
-				});
-			}).then((t) => {
-				return request(`http://www.reddit.com/r/askReddit/hot/.json?count=25&after=${t}`, {json: true}).then((questions) => {
-					this.questions = this.questions.concat(questions.data.children.filter((e) => !e.data.stickied && !e.data.over_18));
-					return questions.data.before;
-				});
-			}).then((t) => {
-				return request(`http://www.reddit.com/r/askReddit/hot/.json?count=25&after=${t}`, {json: true}).then((questions) => {
-					this.questions = this.questions.concat(questions.data.children.filter((e) => !e.data.stickied && !e.data.over_18));
-					return;
-				});
 			});
 		}
+		console.log(this.questions.length);
+		console.log((new Set(this.questions)).size);
 		return msg.say(
-			this.questions.splice(Math.floor(Math.random() * this.questions.length), 1)[0].data.title
+			this.questions.splice(Math.floor(Math.random() * this.questions.length), 1)[0]
 				.replace(/subreddit/gi, 'server')
 				.replace(/reddit/gi, 'Discord')
 				.replace(/\[serious\]/gi, '')

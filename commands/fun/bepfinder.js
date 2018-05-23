a1c43c22e1a719384e27598af2a4f5dade718814
Kuwar Nagpal <kuwar.nagpal@gmail.com>
2018-05-23 14:34:42 -0400

    Aliases for bepfinder

--- commands/fun/bepfinder.js
+++ commands/fun/bepfinder.js
@@ -6,6 +6,7 @@ module.exports = class ReplyCommand extends Command {
 	constructor(client) {
 		super(client, {
 			name: 'bepfinder',
+			aliases: ['bepfriends', 'bepfriend', 'beps'],
 			group: 'fun',
 			memberName: 'bepfinder',
 			description: 'Finds all your bepfriends, people that have the same discriminator as you.',

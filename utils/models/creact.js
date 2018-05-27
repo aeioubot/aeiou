76970e31fd591780e678f56077562f535bc9a4b1
dmbutalsodm <Kuwar.nagpal@gmail.com>
2018-05-27 02:54:35 -0400

    Started workin, set up manager. Lots of work ahead.

--- utils/models/creact.js
+++ utils/models/creact.js
@@ -1,6 +1,11 @@
 const Sequelize = require('sequelize');
 const {Op} = require('sequelize');
 const Database = require('../../database.js');
+const CRManager = require('../classes/cr/CRManager');
+
+//                                              NOTE
+// While creact.js (here) manages the connection between inputs, the CR cache, and the CR database,
+// ../classes/cr/CRManager is the class that serves as the cache and the holder for this live state
 
 const db = Database.db;
 
@@ -14,9 +19,10 @@ const reacts = db.define('reacts', {
 	content: {
 		type: Sequelize.STRING(2000), // eslint-disable-line
 	},
+	type: {
+		type: Sequelize.STRING(20), // eslint-disable-line
+	},
 }, {timestamps: false, charset: 'utf8mb4'});
-
-const allReacts = {};
 
 module.exports = {
 	addReact: async (msg, trigger, content) => {
@@ -63,28 +69,28 @@ module.exports = {
 			}
 		});
 	},
-	findReact: (msg, trigger) => { // from cache
-		if (!allReacts) return null;
-		if (!allReacts[msg.guild.id]) return null;
-		return allReacts[msg.guild.id].find((react) => {
-			return react.trigger === trigger;
-		});
-	},
-	findAllForGuild: (guild) => { // from cache
-		return allReacts[guild] || [];
-	},
-	addToCache: async function(toAdd) {
-		allReacts[toAdd.guild] = allReacts[toAdd.guild] || [];
-		allReacts[toAdd.guild].push({trigger: toAdd.trigger, content: toAdd.content});
-	},
-	removeFromCache: async function(toRemove) {
-		allReacts[toRemove.guild] = allReacts[toRemove.guild] || [];
-		allReacts[toRemove.guild].splice(allReacts[toRemove.guild].findIndex((reaction) => reaction.trigger === toRemove.trigger), 1);
-	},
-	editInCache: async function(toEdit) {
-		allReacts[toEdit.guild] = allReacts[toEdit.guild] || [];
-		allReacts[toEdit.guild].find((reaction) => reaction.trigger === toEdit.trigger).content = toEdit.content;
-	},
+	// findReact: (msg, trigger) => { // from cache
+	// 	if (!allReacts) return null;
+	// 	if (!allReacts[msg.guild.id]) return null;
+	// 	return allReacts[msg.guild.id].find((react) => {
+	// 		return react.trigger === trigger;
+	// 	});
+	// },
+	// findAllForGuild: (guild) => { // from cache
+	// 	return allReacts[guild] || [];
+	// },
+	// addToCache: async function(toAdd) {
+	// 	allReacts[toAdd.guild] = allReacts[toAdd.guild] || [];
+	// 	allReacts[toAdd.guild].push({trigger: toAdd.trigger, content: toAdd.content});
+	// },
+	// removeFromCache: async function(toRemove) {
+	// 	allReacts[toRemove.guild] = allReacts[toRemove.guild] || [];
+	// 	allReacts[toRemove.guild].splice(allReacts[toRemove.guild].findIndex((reaction) => reaction.trigger === toRemove.trigger), 1);
+	// },
+	// editInCache: async function(toEdit) {
+	// 	allReacts[toEdit.guild] = allReacts[toEdit.guild] || [];
+	// 	allReacts[toEdit.guild].find((reaction) => reaction.trigger === toEdit.trigger).content = toEdit.content;
+	// },
 	buildReactCache: async function(guildArray, shardID) {
 		return reacts.findAll({
 			where: {
@@ -94,9 +100,8 @@ module.exports = {
 			},
 		}).then((returnedData) => {
 			let count = 0;
-			returnedData.forEach((reaction) => {
-				if (!allReacts[reaction.guild]) allReacts[reaction.guild] = [];
-				allReacts[reaction.guild].push({ trigger: reaction.trigger, content: reaction.content });
+			returnedData.forEach((reaction) => { // ya yeet not whole notice
+				CRManager.addReaction(reaction.guild, reaction.type, reaction.trigger, reaction.content);
 				count += 1;
 			});
 			return count;

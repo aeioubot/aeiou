acaf23be92aca5ce83876c3fdb0dacec8337de46
dmbutalsodm <Kuwar.nagpal@gmail.com>
2018-05-28 11:16:53 -0400

    CR fixes

--- commands/cr/crlist.js
+++ commands/cr/crlist.js
@@ -30,6 +30,7 @@ module.exports = class CrListCommand extends Command {
 	async run(msg, { argPage, specificReaction }) {
 		const page = parseInt(argPage) || 1;
 		const reactsArray = reactDB.getReacts(msg.guild.id);
+		if (reactsArray.length === 0) return msg.say('There are no custom reactions for this server.');
 		if (specificReaction > 0) {
 			const formattedArray = [`Contents for the trigger **${reactsArray[specificReaction - 1].trigger}**: page ${page} of ${Math.ceil(reactsArray.length / 10)}:\n\`\`\``];
 			let cont = 1;
@@ -40,7 +41,7 @@ module.exports = class CrListCommand extends Command {
 			formattedArray.push(`\`\`\`Use \`${msg.guild.commandPrefix} cr del "${reactsArray[specificReaction - 1].trigger}" <index>\` to delete one of these responses. Use \`all\` for index to delete ALL of these.`);
 			return msg.say(formattedArray.join('\n'));
 		}
-		if (page > Math.ceil(reactsArray.length / 10) || page < 1) return msg.say('That is not a valid page number.');
+		if (page > Math.ceil(reactsArray.length / 10) || page <= 0) return msg.say('That is not a valid page number.');
 		const formattedArray = [`Reaction triggers: page ${page} of ${Math.ceil(reactsArray.length / 10)}:\`\`\``];
 		for (let i = page * 10 - 9; i < page * 10 + 1; i++) {
 			if (reactsArray[i - 1]) formattedArray.push(`${i}. ${reactsArray[i - 1].trigger.padEnd(40, ' ')} ~ ${reactsArray[i - 1].contents.length === 1 ? '' : `${reactsArray[i - 1].contents.length} reactions, `}type: ${reactsArray[i - 1].type}`);
